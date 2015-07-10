193377743e19b2c1b02ab095a64d0b635a5fffdd
Alan Alvarado <alan.alvespi@gmail.com>
2015-07-09 20:47:56 -0500

    estilos

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.configure(function() {
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(express.static(__dirname + '/public'));
-	app.use(express.static(__dirname + '/components'));
-	app.use(express.static(__dirname + '/js'));
-	app.use(express.static(__dirname + '/icons'));
+	//app.use('/components', express.static(__dirname + '/components'));
+	app.use('/js', express.static(__dirname + '/js'));
+	app.use('/icons', express.static(__dirname + '/icons'));
 	app.set('views', __dirname + '/views');
 	app.engine('html', require('ejs').renderFile);
 
